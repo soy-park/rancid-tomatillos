6d2cc633d65a9296ad165b0952641d5c3efa6c2f
Sara Park <sspark106@gmail.com>
2023-05-11 15:06:04 -0600

    Add constructor and header in App.js

--- rancid-tomatillos/src/App.js
+++ rancid-tomatillos/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import movieData from "src/mockData"
+import movieData from './mockData';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Rancid Tomatillos</h1>
       </header>
     </div>
   );
