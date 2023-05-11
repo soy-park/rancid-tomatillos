8835665e4b7f346c6a757ef1376185c7d6de152d
Sara Park <sspark106@gmail.com>
2023-05-11 15:44:38 -0600

    Create JSX elements for movie posters to display on DOM

--- rancid-tomatillos/src/App.js
+++ rancid-tomatillos/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: []
+      movies: movieData.movies
     }
   }
 
--- rancid-tomatillos/src/Movie.js
+++ rancid-tomatillos/src/Movie.js
@@ -1,1 +1,13 @@
+import React from "react";
+import './Movie.css';
+
+const Movie = ({ title, poster, id }) => {
+   return (
+     <div className="movieCard">
+       <img src={poster}/>
+       <h3>{title}</h3>
+     </div>
+   )
+}
+
 export default Movie;
