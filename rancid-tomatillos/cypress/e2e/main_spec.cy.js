8689ca95901d3a7c7bd9bee46826091dc247c92b
Devynne Marshall <devynnem@gmail.com>
2023-05-17 16:23:07 -0600

    feat:Add tests to our Mian page for cypress and tests to our Single Movie page

--- rancid-tomatillos/cypress/e2e/main_spec.cy.js
+++ rancid-tomatillos/cypress/e2e/main_spec.cy.js
@@ -2,22 +2,42 @@ describe('Main page', () => {
 
   beforeEach(() => {
     cy.fixture("movieData.json").as("movieData")
-      .intercept('GET', 'http://localhost:3000', {
-      statusCode: 200,
-      fixture: "movieData"
+      .intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+        statusCode: 200,
+        fixture: "movieData"
       })
-    .visit('http://localhost:3000')
+      .visit('http://localhost:3000');
   });
 
   it('should display a title', () => {
-    cy.contains('h1', 'Rancid Tomatillos')
+    it('should display a title', () => {
+      cy.contains('h1', 'Rancid Tomatillos');
+    });
   });
 
   it('should display all movies', () => {
     cy.get('img').should("be.visible")
-  })
-  // it('should see the Woman King info', () => {
-  //   cy.intercept('POST', 'http://localhost:3000/724495')
-  // })
+      .get('img').should("have.length", 4);
+  });
 
+  it('should render a specific moives details once clicked', () => {
+    cy.get('img').first().click()
+      .get('.single-movie-info');
+  });
+
+  it('should show an error message', () => {
+    {
+      cy.intercept({
+        method: 'GET',
+        url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
+      },
+        {
+          statusCode: 500,
+          body: {
+            message: `Error`
+          }
+        })
+      .visit('http://localhost:3000');
+    }
+  });
 });
--- rancid-tomatillos/cypress/e2e/singleMovie_spec.cy.js
+++ rancid-tomatillos/cypress/e2e/singleMovie_spec.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Single Movie Page', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+      .intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/', {
+      statusCode: 200 })
+      .get('img').first().click()
+      .get('.single-movie-info')
+  });
+
+  it('should see a single movies info displayed', () => {
+      cy.contains('h2', 'Black Adam')
+        .get('.movie-info').contains('Runtime: 125 min');
+  });
+
+  it('should be abe to go back to the main menu', () => {
+      cy.get('button').click()
+        .get('.movies-container');
+  });
+});
