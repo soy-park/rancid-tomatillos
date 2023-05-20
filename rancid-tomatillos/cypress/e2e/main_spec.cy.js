e60bb8147205e232775c1a287ce6bc60b95b6815
Sara Park <sspark106@gmail.com>
2023-05-20 16:21:45 -0600

    feat: Begin cypress testing for form

--- rancid-tomatillos/cypress/e2e/main_spec.cy.js
+++ rancid-tomatillos/cypress/e2e/main_spec.cy.js
@@ -10,17 +10,28 @@ describe('Main page', () => {
   });
 
   it('should display a title', () => {
-    it('should display a title', () => {
-      cy.contains('h1', 'Rancid Tomatillos');
-    });
+    cy.contains('h1', 'Rancid Tomatillos');
   });
+
+  // it('should have a form searchbar that takes in a title', () => {
+  //   cy.get(".search-field").type('woman').should('have.value', 'woman')
+  // });
+
+  // it('should see movies that match title in searchbar', () => {
+  //   cy.get(".search-field").type('woman')
+  //   .get('.movies-container').should('have.length', 1)
+  // });
+
+  // it('should be able to click on form button to return to main page', () => {
+    
+  // });
 
   it('should display all movies', () => {
     cy.get('img').should("be.visible")
       .get('img').should("have.length", 4);
   });
 
-  it('should render a specific moives details once clicked', () => {
+  it('should render a specific movies details once clicked', () => {
     cy.get('img').first().click()
       .get('.single-movie-info');
   });
@@ -29,7 +40,7 @@ describe('Main page', () => {
     {
       cy.intercept({
         method: 'GET',
-        url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
+        url: 'https://rancid-tomatillos.herokuapp.com/api/v2/mvies'
       },
         {
           statusCode: 500,
