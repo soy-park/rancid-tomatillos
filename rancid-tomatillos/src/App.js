69d41ef37ee8a42c202228afa3613020a76ee21b
Devynne Marshall <devynnem@gmail.com>
2023-05-19 21:37:14 -0600

    feat:Add class based component to Form.js

--- rancid-tomatillos/src/App.js
+++ rancid-tomatillos/src/App.js
@@ -5,7 +5,7 @@ import Movies from "./Movies";
 import SingleMovie from "./SingleMovie";
 import Movie from "./Movie";
 import Form from "./Form";
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
@@ -14,7 +14,8 @@ class App extends Component {
       movies: [],
       singleMovie: {},
       value: '',
-      filteredMovies: []
+      filteredMovies: [],
+      searchedMoive: ""
     }
   }
 
@@ -32,6 +33,33 @@ class App extends Component {
       })
       .catch(err => console.log(err))
   }  
+
+  filterMovies = (title) => {
+    console.log(title.toLowerCase())
+    const filteredMovies = this.state.movies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()))
+    this.setState({filteredMovies: filteredMovies, searchedMoive: title})
+    console.log(this.state.filteredMovies)
+    console.log(this.searchedMoive)
+  }
+
+  clearFilteredMovies = () => {
+    this.setState({filteredMovies: [], searchedMoive: ""})
+  }
+
+  // filterMovies = (event) => {
+  //   const desiredMovie = this.state.value.toLowerCase();
+  //   const moviesInLowerCase = this.state.movies.map(movie => movie.title.toLowerCase());
+  //   const searchedMovieTitles = moviesInLowerCase.filter(movie => movie.includes(desiredMovie));
+  //   const searchedMovieTitlesFormatted = searchedMovieTitles.map(movie => movie.split(' ').map(word => word[0].toUpperCase() + word.substr(1)).join(' '));
+  //   const searchedMovies = this.state.movies.filter(movie => movie.title === searchedMovieTitlesFormatted[0])
+  //   this.setState({ 
+  //     movies: this.state.movies,
+  //     singleMovie: {},
+  //     value: event.target.value,
+  //     filteredMovies: this.state.filteredMovies.push(searchedMovies)
+  //   })
+  //   console.log(this.state.filteredMovies)
+  // }
 
   displayMovieInfo = (id) => {
     fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
@@ -52,43 +80,34 @@ class App extends Component {
     this.setState({ movies: this.state.movies, singleMovie: {} })
   }
   
-  handleChange = (event) => {
-    event.preventDefault();
-    this.setState({ 
-      movies: this.state.movies,
-      singleMovie: {},
-      value: event.target.value,
-      filteredMovies: []
-    })
-  }
+  // handleChange = (event) => {
+  //   event.preventDefault();
+  //   this.setState({ 
+  //     movies: this.state.movies,
+  //     singleMovie: {},
+  //     value: event.target.value,
+  //     filteredMovies: []
+  //   })
+  // }
   
-  filterMovies = (event) => {
-    const desiredMovie = this.state.value.toLowerCase();
-    const moviesInLowerCase = this.state.movies.map(movie => movie.title.toLowerCase());
-    const searchedMovieTitles = moviesInLowerCase.filter(movie => movie.includes(desiredMovie));
-    const searchedMovieTitlesFormatted = searchedMovieTitles.map(movie => movie.split(' ').map(word => word[0].toUpperCase() + word.substr(1)).join(' '));
-    const searchedMovies = this.state.movies.filter(movie => movie.title === searchedMovieTitlesFormatted[0])
-    this.setState({ 
-      movies: this.state.movies,
-      singleMovie: {},
-      value: event.target.value,
-      filteredMovies: this.state.filteredMovies.push(searchedMovies)
-    })
-    console.log(this.state.filteredMovies)
-  }
 
   render() {
+    const movieData = this.state.searchedMoive ? this.state.filteredMovies : this.state.movies
     return (
       <main className="App">
           <h1>Rancid Tomatillos</h1>
-          <Route exact path="/" render={() => <Form value={this.state.value} id={this.state.filteredMovies[0].id} handleChange={this.handleChange} filterMovies={this.filterMovies}/>} />
-          <Route exact path="/" render={() => <Movies name="movies" movies={this.state.movies} singleMovie={this.state.singleMovie} displayMovieInfo={this.displayMovieInfo}/>} />
+          <Route exact path="/" render={() => <Form filterMovies={this.filterMovies} clearFilteredMovies={this.clearFilteredMovies}/>} />
+          {/* <Route exact path="/" render={() => <Form value={this.state.value} id={this.state.filteredMovies} handleChange={this.handleChange} filterMovies={this.filterMovies}/>} /> */}
+          <Switch>
+          <Route exact path="/" render={() => <Movies name = "movies" movies={movieData} displayMovieInfo={this.displayMovieInfo}/>} />
+          {/* <Route exact path="/" render={() => <Movies name="movies" movies={this.state.movies} singleMovie={this.state.singleMovie} displayMovieInfo={this.displayMovieInfo}/>} /> */}
           <Route path="/:id" render={({ match }) => {
             const movieID = match.params.id;
             this.displayMovieInfo(movieID);
             return (<SingleMovie movie={this.state.singleMovie} displayMainPage={this.displayMainPage} />
             )
           }} />
+          </Switch>
       </main>
     )
   } 
--- rancid-tomatillos/src/Form.js
+++ rancid-tomatillos/src/Form.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 
-const Form = ({ id, value, handleChange, filterMovies }) => {
-    return (
-        <form>
-            <input 
-                type="text" 
-                name="searchbar" 
-                placeholder="Search Movie" 
-                onChange={handleChange}
-            />
-            <Link to={`/${id}`} key={`${id}`}><input 
-                type="submit" 
-                value="Submit" 
-                onClick={(event) => filterMovies(event)}
-            /></Link>
-        </form>
-    )
- }
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+        };
+    };
+
+    handleChange = (event) => {
+        
+        this.setState({ [event.target.name]: event.target.value }, () => this.props.filterMovies(this.state.title))
+        console.log(this.state.title)
+        // console.log(this.props.filterMovies())
+    }
+
+   
+
+    render() {
+        return (
+            <div>
+                <input 
+                    type="text" 
+                    placeholder="Search Movie Title" 
+                    value={this.state.title}
+                    name="title" 
+                    onChange={(event) => this.handleChange(event)}
+                    className="search-field"
+                />
+                <button className="button" onClick={(event) => this.clearInputs(event)}>
+                Clear
+                </button>
+                {/* <Link to={`/${id}`} key={`${id}`}><input 
+                    type="submit" 
+                    value="Submit" 
+                    onClick={(event) => filterMovies(event)}
+                /></Link> */}
+            </div>
+        )
+     }
+
+    }
+
+
+
 
 export default Form;
